875263fe136f8e4115d1b6b2bbd22793947eb228
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2023-11-06 12:00:10 +1100

    v0.1.2 - Revert to Older Method

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 run:
 	rustup update
 	cargo update
-	cargo build --release --target=wasm32-unknown-unknown
+	cargo clean
+	cargo build --release --target=wasm32-unknown-unknown -v
 	deno run --allow-read --allow-write main.ts
--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { encodeHex } from 'https://deno.land/std@0.205.0/encoding/hex.ts'
-import { parse, stringify } from 'https://deno.land/std@0.205.0/toml/mod.ts'
+import { parse } from 'https://deno.land/std@0.205.0/toml/mod.ts'
 await Deno.mkdir('output/', { recursive: true })
 
 type Cargo = {
@@ -12,12 +12,7 @@ type Cargo = {
 
 const hashes = JSON.parse(await Deno.readTextFile('output/hashes.json').catch(() => '{}')) as Record<string, string | undefined>
 const hash = encodeHex(await crypto.subtle.digest('SHA-256', await Deno.readFile('target/wasm32-unknown-unknown/release/deterministic_rust.wasm')))
+hashes[ await Deno.realPath('./') + ' ' + (parse(await Deno.readTextFile('Cargo.toml')) as Cargo).package.version ] = hash
 
-const cargo = parse(await Deno.readTextFile('Cargo.toml')) as Cargo
-hashes[ await Deno.realPath('./') + ' ' + cargo.package.version ] = hash
-cargo.package.version = cargo.package.version.slice(0, cargo.package.version.lastIndexOf('.') + 1)
-	+ (parseInt(cargo.package.version.slice(cargo.package.version.lastIndexOf('.') + 1)) + 1)
-
-Deno.writeTextFile('Cargo.toml', stringify(cargo))
 Deno.writeTextFile('output/hashes.json', JSON.stringify(hashes, undefined, '\t'))
 Deno.rename('target/wasm32-unknown-unknown/release/deterministic_rust.wasm', `output/${hash}.wasm`)
