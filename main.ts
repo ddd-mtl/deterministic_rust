ad3ef46f7ac4eddc20e2b81b631305471d294d4d
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2024-07-01 17:53:40 +1000

    v0.2.0 - Testing changes in cargo

--- main.ts
+++ main.ts
@@ -1,18 +1,16 @@
-import { encodeHex } from 'https://deno.land/std@0.205.0/encoding/hex.ts'
-import { parse } from 'https://deno.land/std@0.205.0/toml/mod.ts'
-await Deno.mkdir('output/', { recursive: true })
+import { encodeBase64Url } from "@std/encoding";
 
-type Cargo = {
-	package: {
-		name: string,
-		version: string,
-		edition: string,
-	}
-}
+await Deno.mkdir("output/" + Deno.build.os + "/", { recursive: true });
 
-const hashes = JSON.parse(await Deno.readTextFile('output/hashes.json').catch(() => '{}')) as Record<string, string | undefined>
-const hash = encodeHex(await crypto.subtle.digest('SHA-256', await Deno.readFile('target/wasm32-unknown-unknown/release/deterministic_rust.wasm')))
-hashes[ await Deno.realPath('./') + ' ' + (parse(await Deno.readTextFile('Cargo.toml')) as Cargo).package.version ] = hash
-
-Deno.writeTextFile('output/hashes.json', JSON.stringify(hashes, undefined, '\t'))
-Deno.rename('target/wasm32-unknown-unknown/release/deterministic_rust.wasm', `output/${hash}.wasm`)
+await Deno.rename(
+  "target/wasm32-unknown-unknown/release/deterministic_rust.wasm",
+  "output/" + Deno.build.os + "/" +
+    encodeBase64Url(
+      await crypto.subtle.digest(
+        "SHA-256",
+        await Deno.readFile(
+          "target/wasm32-unknown-unknown/release/deterministic_rust.wasm",
+        ),
+      ),
+    ) + ".wasm",
+);
